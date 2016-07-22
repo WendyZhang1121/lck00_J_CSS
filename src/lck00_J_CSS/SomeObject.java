1b6985beb3edff00de34db39fef47d98317973ed
Yuqi Zhang <yanwuzhidie@sina.com>
2016-07-22 23:06:16 +0100

    Creation

---
+++ src/lck00_J_CSS/SomeObject.java
@@ -0,0 +1,10 @@
+package lck00_J_CSS;
+
+public class SomeObject {
+	private static final Object lock = new Object(); 
+	public static void changeValue() {
+		synchronized (lock) { // Locks on the private Object 
+			// ...
+		} 
+	}
+}
